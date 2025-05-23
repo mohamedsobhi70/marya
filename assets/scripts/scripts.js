092989973f8da0d984059f5d762ed6451af54c9d
mohamedsobhi70 <sobhi7070@gmail.com>
2025-05-23 22:48:09 +0300

    add owl carousel

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,3 +1,25 @@
+
+const sections = document.querySelectorAll('.section-item');
+const navLinks = document.querySelectorAll('.nav-link');
+
+window.addEventListener('scroll', () => {
+    let current = '';
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (scrollY >= (sectionTop - sectionHeight / 3)) {
+            current = section.getAttribute('id');
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.parentElement.classList.remove('active');
+        if (link.getAttribute('href') === `#${current}`) {
+            link.parentElement.classList.add('active');
+        }
+    });
+});
+
 document.querySelectorAll(".nav-item").forEach((item) => {
 
     item.addEventListener("click", () => {
@@ -7,3 +29,41 @@ document.querySelectorAll(".nav-item").forEach((item) => {
         item.classList.add("active")
     })
 })
+
+
+
+$('.services-slider').owlCarousel({
+    nav: false,
+    margin: 53,
+    loop: false,
+    autoplay: true,
+    rtl: true,
+    responsive: {
+        0: {
+            items: 1, margin: 20
+        },
+        480: {
+            items: 2, margin: 20,
+        },
+        787: {
+            items: 3, margin: 20,
+        },
+        912: {
+            items: 4, margin: 20,
+        },
+        1200: {
+            items: 5, margin: 53,
+        },
+        1300: {
+            items: 6, margin: 53,
+        }
+    }
+});
+
+$('.next-service').click(function () {
+    $('.services-slider').trigger('next.owl.carousel');
+});
+
+$('.prev-service').click(function () {
+    $('.services-slider').trigger('prev.owl.carousel');
+});
